cb1fcaaa4e8a8a5cdc8b7df6202664413cee7b98
Jiayao She <48703327+garysheh@users.noreply.github.com>
2024-04-30 16:20:54 +0800

    UI update

--- SmartTrade/LoginViewController.swift
+++ SmartTrade/LoginViewController.swift
@@ -9,13 +9,19 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func loginClicked(_ sender: UIButton) {
+        
+    }
+    
     /*
     // MARK: - Navigation
 
--- SmartTrade/RegisterViewController.swift
+++ SmartTrade/RegisterViewController.swift
@@ -8,14 +8,20 @@
 import UIKit
 
 class RegisterViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func signupClicked(_ sender: UIButton) {
+    }
+    
     /*
     // MARK: - Navigation
 
