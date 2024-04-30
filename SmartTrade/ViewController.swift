855e1351fbec6b0bb75373eeac4e63f835681f22
Jiayao She <48703327+garysheh@users.noreply.github.com>
2024-04-30 09:08:13 +0800

    Initial Commit

---
+++ SmartTrade/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartTrade
+//
+//  Created by Gary She on 2024/4/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
