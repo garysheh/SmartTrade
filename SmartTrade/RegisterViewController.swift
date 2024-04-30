f1b25035be1155b56012c0aea5195df352034e9f
LeungKwokFan <klkouson1204@gmail.com>
2024-04-30 17:55:44 +0800

    Firebase add.

--- SmartTrade/RegisterViewController.swift
+++ SmartTrade/RegisterViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
     
@@ -20,6 +21,22 @@ class RegisterViewController: UIViewController {
     }
     
     @IBAction func signupClicked(_ sender: UIButton) {
+        guard let email = emailTextField.text else {return}
+        guard let password = passwordTextField.text else {return}
+        
+        Auth.auth().createUser(withEmail:email, password: password) { firebaseResult, error in
+            if let e = error{
+                print("error")
+            }
+            else{
+                self.performSegue(withIdentifier:"goToNext", sender: self)
+            }
+        }
+        
+        
+        
+        
+        
     }
     
     /*
