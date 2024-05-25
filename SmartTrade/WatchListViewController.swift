ad00749c7420710ecd035dde8db906cf59d9d194
Jiayao She <48703327+garysheh@users.noreply.github.com>
2024-05-26 00:21:22 +0800

    Tab screen created

--- SmartTrade/WatchListViewController.swift
+++ SmartTrade/WatchListViewController.swift
@@ -5,4 +5,12 @@
 //  Created by Gary She on 2024/5/25.
 //
 
-import Foundation
+import UIKit
+
+class WatchListViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.isHidden = true
+    }
+    
+}
