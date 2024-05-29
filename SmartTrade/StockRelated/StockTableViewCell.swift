27bbddb5058788d96ec6fe19c4eafb5f6bf986ed
Jiayao She <48703327+garysheh@users.noreply.github.com>
2024-05-30 00:35:05 +0800

    Stock price fetch

--- SmartTrade/StockRelated/StockTableViewCell.swift
+++ SmartTrade/StockRelated/StockTableViewCell.swift
@@ -4,12 +4,12 @@ class StockTableViewCell: UITableViewCell {
     @IBOutlet weak var assetNameLabel: UILabel!
     @IBOutlet weak var assetSymbolLabel: UILabel!
     @IBOutlet weak var assetTypeLabel: UILabel!
+    @IBOutlet weak var assetHighLabel: UILabel!
     
     func configure(with searchResult: SearchResult) {
         assetNameLabel.text = "\(String(describing: searchResult.price))"
         assetSymbolLabel.text = searchResult.symbol
-        assetTypeLabel.text = "\(String(describing: searchResult.high))"
-            .appending(" ")
-            .appending("\(String(describing: searchResult.low))")
+        assetTypeLabel.text = "HIGH: \(String(describing: searchResult.low))"
+        assetHighLabel.text = "LOW: \(String(describing: searchResult.high))"
     }
 }
