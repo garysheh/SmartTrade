7e25e1a513af41c49563eac9ee10592f12bf5070
Jiayao She <48703327+garysheh@users.noreply.github.com>
2024-06-01 17:02:26 +0800

    Accounts page setup

---
+++ SmartTrade/AccountsPage/AccountsViewController.swift
@@ -0,0 +1,46 @@
+//
+//  AccountsViewController.swift
+//  SmartTrade
+//
+//  Created by Gary She on 2024/6/1.
+//
+
+import UIKit
+
+class AccountsViewController: UIViewController {
+    
+    @IBOutlet weak var shareCounts: UILabel!
+    
+    @IBOutlet weak var marketValue: UILabel!
+    
+    @IBOutlet weak var costValue: UILabel!
+    
+    @IBOutlet weak var portRate: UILabel!
+    
+    @IBOutlet weak var todayReturnRate: UILabel!
+    
+    @IBOutlet weak var todayReturnValue: UILabel!
+    
+    @IBOutlet weak var totalReturnValue: UILabel!
+    
+    @IBOutlet weak var totalReturnRate: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
