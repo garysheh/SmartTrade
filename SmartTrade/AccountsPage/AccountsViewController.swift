c00923968b01a8b93193dc3049ef2ec1e42cdc72
LeungKwokFan <klkouson1204@gmail.com>
2024-06-08 17:38:31 +0800

    Market value set.

--- SmartTrade/AccountsPage/AccountsViewController.swift
+++ SmartTrade/AccountsPage/AccountsViewController.swift
@@ -35,9 +35,13 @@ class AccountsViewController: UIViewController {
     
     @IBOutlet weak var totalReturnRate: UILabel!
     
+    @IBOutlet weak var detailButton: UIButton!
     
     
     private var stockData: [String: Double] = [:]
+    private var stockKeys: [String] = []
+    private var subscribers = Set<AnyCancellable>()
+    private var searchResults: [SearchResult] = []
     
     let cusColors: [UIColor] = [
         UIColor(red: 13/255.0, green: 126/255.0, blue: 156/255.0, alpha: 1.0),
@@ -56,28 +60,54 @@ class AccountsViewController: UIViewController {
     
     
     override func viewWillAppear(_ animated: Bool) {
-            super.viewWillAppear(animated)
-            setupCharts()
+        super.viewWillAppear(animated)
+        setupChartsData()
+        setupShareCounts()
+        calculateTotalValue()
         }
         
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+
+    //get the data from the database
+    private func getHoldingData(completion: @escaping ([String: Double]) -> Void) {
+        let db = Firestore.firestore()
+        let email = Auth.auth().currentUser?.email
+        var stockHoldings: [String: Double] = [:] //the position
         
-    private func setupCharts() {
+
+        db.collection("Holdings").document(email!).getDocument { (document, error) in
+            if let document = document, document.exists {
+                var holdings = document.data()?["holdings"] as? [[String: Any]] ?? []
+                for holding in holdings {
+                    if let stockCode = holding["stockCode"] as? String, let shares = holding["shares"] as? Double {
+                        stockHoldings[stockCode] = shares
+                        
+                    }
+                }
+                completion(stockHoldings)
+            } else {
+                completion([:])
+            }
+        }
+    }
+    
+    
+    //update the chart when entering this page
+    private func setupChartsData() {
         // clean the chart before
         ChartsView.subviews.forEach { $0.removeFromSuperview() }
 
-        // update the chart and draw
+        // update the chart and data
         getHoldingData { holdingData in
             self.drawPieChart(with: holdingData)
-            let totalShares = holdingData.values.reduce(0, +)
-            self.shareCounts.text = "\(totalShares)"
-            self.shareCounts.textAlignment = .left
+            
         }
-        
     }
+    
+    
 
     private func drawPieChart(with holdingData: [String: Double]) {
         
@@ -98,38 +128,86 @@ class AccountsViewController: UIViewController {
         let pieChartData = PieChartData(dataSet: dataSet)
         pieChartView.data = pieChartData
         
-        
     }
-
-    private func getHoldingData(completion: @escaping ([String: Double]) -> Void) {
-        let db = Firestore.firestore()
-        let email = Auth.auth().currentUser?.email
-        var stockHoldings: [String: Double] = [:] //the position
+    
+    
+    //update the sharescount
+    private func setupShareCounts(){
         
-        
-
-        db.collection("Holdings").document(email!).getDocument { (document, error) in
-            if let document = document, document.exists {
-                var holdings = document.data()?["holdings"] as? [[String: Any]] ?? []
-                for holding in holdings {
-                    if let stockCode = holding["stockCode"] as? String, let shares = holding["shares"] as? Double {
-                        stockHoldings[stockCode] = shares
-                        
-                    }
-                }
-                completion(stockHoldings)
-            } else {
-                completion([:])
-            }
+        getHoldingData { holdingData in
+            //set the share count
+            let totalShares = holdingData.values.reduce(0, +)
+            self.shareCounts.text = "\(totalShares)"
+            
         }
     }
     
+    private func calculateTotalValue() {
+        getHoldingData { holdingData in
+            let apiService = APIService()
+            let publishers = holdingData.keys.map { apiService.fetchSymbolsPublisher(symbol: $0) }
+            var stockValues: [String: Double] = [:]
+            self.stockKeys = Array(holdingData.keys)
+                        
+            DispatchQueue.main.async {
+                
+                print(self.stockKeys)
+                let publishers = self.stockKeys.map { apiService.fetchSymbolsPublisher(symbol: $0) }
+                
+                Publishers.MergeMany(publishers)
+                    .map { data -> SearchResult? in
+                        if let searchResults = try? JSONDecoder().decode(SearchResults.self, from: data) {
+                            return searchResults.globalQuote
+                        }
+                        return nil
+                    }
+                    .collect()
+                    .receive(on: RunLoop.main)
+                    .sink { completion in
+                        switch completion {
+                        case .failure(let error):
+                            print(error.localizedDescription)
+                        case .finished:
+                            break
+                        }
+                    } receiveValue: { searchResults in
+                        self.searchResults = searchResults.compactMap { $0 }
+                        
+                        // Calculate the total value
+                        var totalValue: Double = 0
+                        for (symbol, shares) in holdingData {
+                            if let searchResult = self.searchResults.first(where: { $0.symbol == symbol }) {
+                                let currentPrice = Double(searchResult.price ?? "0") ?? 0
+                                let value = Double(shares) * currentPrice
+                                totalValue += value
+                                stockValues[symbol] = value
+                            }
+                        }
+                        
+                        // Set the total value text
+                        self.marketValue.text = String(format: "%.2f", totalValue)
+                        print("Stock Values: \(stockValues)")
+                    }
+                    .store(in: &self.subscribers)
+            }
+            
+        }
+    }
+    
+
+
+    
+    
+    
+    
     
     
     
     
 
 
+    @IBAction func detailButtonTapped(_ sender: Any) {
+    }
     
 
     /*
