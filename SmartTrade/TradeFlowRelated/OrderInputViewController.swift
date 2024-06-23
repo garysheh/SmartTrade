9150c87b6da2c349a96ca306e1babbee22833a79
Jiayao She <48703327+garysheh@users.noreply.github.com>
2024-06-23 21:33:47 +0800

    Trading flow: order input page draft 2

--- SmartTrade/TradeFlowRelated/OrderInputViewController.swift
+++ SmartTrade/TradeFlowRelated/OrderInputViewController.swift
@@ -8,22 +8,65 @@
 import UIKit
 
 class OrderInputViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+    @IBOutlet weak var limitLabel: UILabel!
+    @IBOutlet weak var marketLabel: UILabel!
+    @IBOutlet weak var youPayField: UILabel!
+    @IBOutlet weak var paymentField: UILabel!
+    @IBOutlet weak var priceOrderType: UILabel!
     
+        private var currentViewController1: UIViewController?
+        private var currentViewController2: UIViewController?
+        
+        private var marketOrderViewController: UIViewController?
+        private var limitOrderViewController: UIViewController?
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            setupRoundedLabel(labels: [youPayField,paymentField])
+            highlightMarketOrder()
+            setupGestureRecognizers()
+            
+        }
+        
+        @objc private func marketOrderTapped() {
+            highlightMarketOrder()
+            updateOrderTypeLabel(to: "Market Order")
+        }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+        @objc private func limitOrderTapped() {
+            highlightLimitOrder()
+            updateOrderTypeLabel(to: "Limit Order")
+        }
+    
+        private func updateOrderTypeLabel(to orderType: String) {
+            priceOrderType.text = orderType
+        }
+        
+        private func highlightMarketOrder() {
+            marketLabel.textColor = .white
+            limitLabel.textColor = UIColor(red: 142/255.0, green: 142/255.0, blue: 147/255.0, alpha: 1.0) // Default color
+        }
+        
+        private func highlightLimitOrder() {
+            limitLabel.textColor = .white
+            marketLabel.textColor = UIColor(red: 142/255.0, green: 142/255.0, blue: 147/255.0, alpha: 1.0) // Default color
+        }
+        
+        private func setupGestureRecognizers() {
+            let marketOrderTapGesture = UITapGestureRecognizer(target: self, action: #selector(marketOrderTapped))
+            marketLabel.isUserInteractionEnabled = true
+            marketLabel.addGestureRecognizer(marketOrderTapGesture)
+            
+            let limitOrderTapGesture = UITapGestureRecognizer(target: self, action: #selector(limitOrderTapped))
+            limitLabel.isUserInteractionEnabled = true
+            limitLabel.addGestureRecognizer(limitOrderTapGesture)
+        }
+    
+        private func setupRoundedLabel(labels: [UILabel]) {
+            for label in labels {
+                label.layer.cornerRadius = 10 // Adjust the radius as needed
+                label.layer.masksToBounds = true
+            }
+        }
 
 }
