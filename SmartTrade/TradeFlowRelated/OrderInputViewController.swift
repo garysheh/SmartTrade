85656918c1eff76d0f562cbb7f53a91f0c3b38b5
Jiayao She <48703327+garysheh@users.noreply.github.com>
2024-06-23 21:33:11 +0800

    Trading flow: order input page draft

---
+++ SmartTrade/TradeFlowRelated/OrderInputViewController.swift
@@ -0,0 +1,29 @@
+//
+//  OrderInputViewController.swift
+//  SmartTrade
+//
+//  Created by Gary She on 2024/6/22.
+//
+
+import UIKit
+
+class OrderInputViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
